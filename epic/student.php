e86e1202620c2989879ee21da69260b9cc6c4692
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-30 13:55:12 -0700

    started student persona student.php

---
+++ epic/student.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Student Persona</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<h3>Kevin</h3>
+		<p>Kenneth is a 26 year old male who uses a 17" 2011 macbook pro. He has a diverse background, both professionally and personally, and wants to publish relevant articles about his hobbies to share with his family and friends.</p>
+		<h3>User Story</h3>
+		<p>As a user, I want to publish simple, modern, multi-media articles about my hobbies</p>
+		<h3>Use Case</h3>
+		<p>Ken wants to publish an "About Me" as their first article</p>
+		<h3>Preconditions</h3>
+		<p>Ken has subscribed and is already logged into her Medium account</p>
+		<h3>Postconditions</h3>
+		<p>Ken's article "About Me" is live</p>
+		<h3>Interaction Flow</h3>
+		<ol>
+			<li>Ken clicks their profile image in the top right of the screen</li>
+			<li>Medium provides a drop-down menu</li>
+			<li>Ken clicks "New story"</li>
+			<li>Medium navigates to http://medium.com/new-story</li>
+			<li>Ken writes them "About Me" and clicks "Publish"</li>
+			<li>Medium presents a pop-up asking them if she is "Ready to publish?", to enter tags, connect to their social media... etc.</li>
+			<li>Ken clicks "Publish"</li>
+			<li>Medium redirects them to https://medium.com/@theirUsername/About-Me-articleId</li>
+		</ol>
+	</body>
+</html>
