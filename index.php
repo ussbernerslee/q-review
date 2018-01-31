d2d90735493ca9c70fc25ffa5a142f3d39a49298
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-31 14:03:50 -0700

    updated links on index.php

--- index.php
+++ index.php
@@ -8,8 +8,11 @@
 		<h1>Q-Review Documentation Menu</h1>
 		<h3>Epic</h3>
 		<ol>
-			<li><a href="epic/student.php">Student Persona</a></li>
-			<li><a href="epic/proctor.php">Proctor Persona</a></li>
-		</ol>
+			<li><a href="epic/student.php">Student Persona Draft</a></li>
+			<li><a href="epic/proctor.php">Proctor Persona Draft</a></li>
+			<li><a href="epic/kens-conceptual-model.php">Conceptual Model Draft</a></li>
+			<li><a href="epic/executive-summary.php">Executive Summary Draft</a>
+			<li><a href="epic/user-interaction-flow.php">Interaction Flow Draft</a></li>
+			<li><a href="epic/user-goals.php">User Goals Draft</a></li>
 	</body>
 </html>
