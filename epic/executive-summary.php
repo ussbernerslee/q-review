8cb7152991367060ec8247969b05e42fb4304e88
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-31 13:32:44 -0700

    added executive summary file

---
+++ epic/executive-summary.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Executive Summary</title>
+	</head>
+	<body>
+		<h1>Executive Summary</h1>
+		<p>Problem</p>
+		<p>Solution</p>
+		<p>Q Review is a competitive question and answer review game that allows an instructor to create custom flash-cards with unified difficulty, point values, and category tags. With this system of identification, Q Review will allow the instructor to generate a Game Board with any number of categories and questions with increasing difficulty. Q Review then allows multiple Student users to simultaneously join the Game that the instructor has started. Q Review will allow the instructor to moderate a review game, where students must race to hit the spacebar in order to be selected to answer questions in order to be awarded points. Q Review will have the ability to accept written answers from students as well.</p>
+	</body>
+</html>
