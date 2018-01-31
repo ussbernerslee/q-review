26182d889463d67193ff984e19a000af95e869a3
freddy-crawford <freddy_crawford@yahoo.com>
2018-01-31 13:54:59 -0700

    added user goals file

---
+++ epic/user-goals.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>User Goals</title>
+	</head>
+	<body>
+<h2>User Goals</H2>
+		<h4>By using the Q Review site as a "proctor" user can accomplish these goals</h4>
+		<ul>
+			<li>User can set up a classroom based interactive learning game mimicked after popular television show Jeopardy.</li>
+			<li>User has ability Ability to set any question base needed according to any topic or type of class and choose topic types and set values based on difficulty of questions.</li>
+			<li>User can Easily track data from past games to show student interaction and information retention for possible review topics.</li>
+			<li>User can easily proctor a game and can make on the fly changes to questions in game as needed.</li>
+			<li>User can help students with in class learning by providing information in a fun and interactive game.</li>
+			<li></li>
+		</ul>
+
+
+
+</body>
+</html>
