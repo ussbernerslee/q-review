a8e803d5f4bd36782b224a1d32ca77c7172f0264
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-30 18:57:33 -0700

    running through a first draft of an entity and attribute list for the backend, this is saved in my personal file kens-conceptual-model which we can delete once we finalize the model

---
+++ epic/kens-conceptual-model.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Ken's Conceptual Model</title>
+	</head>
+	<body>
+		<h1>Entity: Profile</h1>
+		<h3>Attribute: ProfileId</h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+
+		<h1>Entity: Category</h1>
+		<h3>Attribute: categoryId</h3>
+		<h3>Attribute: categoryProfileId</h3>
+		<h3>Attribute: categoryTag</h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+
+		<h1>Entity: Card</h1>
+		<h3>Attribute: cardId</h3>
+		<h3>Attribute: cardCategoryId</h3>
+		<h3>Attribute: cardQuestion</h3>
+		<h3>Attribute: cardAnswer</h3>
+		<h3>Attribute: cardValue</h3>
+		<h3>Attribute: cardDoubleOrNothing</h3>
+		<h3>Attribute: cardFinalWager</h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+
+		<h1>Entity: Board</h1>
+		<h3>Attribute: boardId</h3>
+		<h3>Attribute: boardProfileId</h3>
+		<h3>Attribute: board</h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+		<h3>Attribute: </h3>
+
+		<h1></h1>
+	</body>
+</html>
