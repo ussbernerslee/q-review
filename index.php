472b28d2b00729416ae8cec4cb4be3e8e7d283c3
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-30 19:17:53 -0700

    added links to index.php for navigating through Epic and other Documentation

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Navigation for Capstone Documentation</title>
+	</head>
+	<body>
+		<h1>Q-Review Documentation Menu</h1>
+		<h3>Epic</h3>
+		<ol>
+			<li><a href="epic/student.php">Student Persona</a></li>
+			<li><a href="epic/proctor.php">Proctor Persona</a></li>
+		</ol>
+	</body>
+</html>
