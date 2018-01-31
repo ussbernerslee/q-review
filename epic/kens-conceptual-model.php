93c86687ff5bda8e5123cae9213aec3a2ae8ed36
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-30 19:09:39 -0700

    running through a first draft of an entity and attribute list for the backend, this is saved in my personal file kens-conceptual-model which we can delete once we finalize the model

--- epic/kens-conceptual-model.php
+++ epic/kens-conceptual-model.php
@@ -6,12 +6,11 @@
 	</head>
 	<body>
 		<h1>Entity: Profile</h1>
-		<h3>Attribute: ProfileId</h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
+		<h3>Attribute: profileId</h3>
+		<h3>Attribute: profileAuthorization</h3>
+		<h3>Attribute: profileHash</h3>
+		<h3>Attribute: profileSalt</h3>
+		<h3>Attribute: profileUsername</h3>
 
 		<h1>Entity: Category</h1>
 		<h3>Attribute: categoryId</h3>
@@ -37,15 +36,20 @@
 		<h1>Entity: Board</h1>
 		<h3>Attribute: boardId</h3>
 		<h3>Attribute: boardProfileId</h3>
-		<h3>Attribute: board</h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
+		<h3>Attribute: boardSubject</h3>
+		<h3>Attribute: boardCategories</h3>
+		<h3>Attribute: boardCards</h3>
 		<h3>Attribute: </h3>
 		<h3>Attribute: </h3>
 		<h3>Attribute: </h3>
 		<h3>Attribute: </h3>
 
-		<h1></h1>
+		<h1>Entity: Play-By-Play</h1>
+		<h3>Attribute: playByPlayId</h3>
+		<h3>Attribute: </h3>
+
+		<h1>Entity: Score</h1>
+		<h3>Attribute: </h3>
+
 	</body>
 </html>
