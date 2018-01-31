2ef527bb0ae79564323a6497d6f2adadffc25baf
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-30 18:37:01 -0700

    profile Proctor-completed up to partial interaction flow

--- epic/proctor.php
+++ epic/proctor.php
@@ -6,17 +6,18 @@
 
 	</head>
 	<body>
+		<div>
 		<h2>Personas, Use Cases, and Interaction Flow</h2>
 		<h3>Persona:</h3>
 		<ul>
-			<li><strong>Name</strong>: "Deep Dive Dylan"</li>
-			<li><strong>Personality</strong>: </li>
+			<li><strong>Name</strong>: DeepDiveDylan</li>
+			<li><strong>Personality</strong>: Enthusiastic, energetic, brilliant, generous, kind to his students and co-workers. Loves his job, willing to "go there" to investigate any question his students ask.</li>
 			<li><strong>Gender</strong>: Male</li>
 			<li><strong>Age</strong>: 35</li>
 			<li><strong>Technology</strong>: mac, pc, linux...but happiest on mac
 				<ul>
 					<li><strong>Device</strong> Newest Macbook Pro with Touch Bar...The 15-inch MacBook Pro features a powerful Radeon Pro discrete GPU in every configuration. Manufactured with a 14 nm process, Radeon Pro graphics combine outstanding power with outstanding power efficiency. And now 4GB of GDDR5 memory comes standard on the top configuration, giving you fluid, real-time performance for pro tasks like rendering 3D titles in Final Cut Pro X. Every 13-inch model features powerful integrated graphics with 64MB of embedded DRAM, which accelerates graphics tasks. That means more time for what matters most — creating amazing work (and mining Bitcoin)</li>
-					<li><strong>Proficiency</strong>Captain</li>
+					<li><strong>Proficiency: </strong>extreme...Captain</li>
 					<li><strong>Love/Hate</strong> Loves, Loves, LOVES!  Checks Slack continuously. Keeps track of Bitcoin shares.</li>
 				</ul>
 			</li>
@@ -37,30 +38,25 @@
 			<li><strong>Title: </strong>Proctoring a round of game-play:</li>
 			<li><strong>Name of the "actor, user or Persona, and their role: </strong> DeepDiveDylan, Captain/Proctor of game</li>
 			<li><strong>Usage Preconditions: </strong>DeepDiveDylan must be signed into CNM, using his CNM UserId and password</li>
-			<li><strong>Usage Postconditions:</strong>DeepDiveDylan has successfully set up a game, proctored it, and has the scores of his players/students</li>
+			<li><strong>Usage Postconditions:</strong>DeepDiveDylan has successfully set up a game, proctored it, has the scores of his players/students, and knows who the winner is.(may have entered new questions and set tags and value)</li>
 			<li><strong>Interaction Flow(s), including alternates, if appropriate</strong></li>
-			<li><strong>Frequency of Use: </strong> once-a-week</li>
+			<li><strong>Frequency of Use: </strong>at least once-per-week</li>
 		</ul>
 		<h3>Interaction Flow:</h3>
 		<!--Enter each after the </strong> and before the </li>-->
 		<ul>
-			<li><strong>User Action 1</strong> Alicia enters her username and password.</li>
-			<li><strong>System Response 1</strong> Medium logs her in.</li>
-			<li><strong>User Action 2</strong> Alicia clicks that she wants to post an article.</li>
-			<li><strong>System Response 2</strong> Medium opens a box in which she can write or cut and paste her article, with an area for title and for content.</li>
-			<li><strong>User Action 3</strong> Alicia enters her article, with title and content.</li>
-			<li><strong>System Response 3</strong> Medium saves her article and displays it for other members to read.</li>
-			<li><strong>User Action 4</strong> Alicia clicks the logout button.</li>
-			<li><strong>System Response 4</strong> Medium logs Alicia out.</li>
+			<li><strong>User Action 1</strong> DeepDiveDylan enters his username and password.</li>
+			<li><strong>System Response 1</strong> CNM logs him in.</li>
+			<li><strong>User Action 2</strong> DeepDiveDylan navigates to the Q-Review Website and clicks the button to log-in as a Proctor.</li>
+			<li><strong>System Response 2</strong> Q-Review recognizes that he is allowed to be a proctor, so opens a page that gives him the choice to enter new questions, edit questions, or set up a game.</li>
+			<li><strong>User Action 3</strong> DeepDiveDylan chooses to set up a game, and clicks on this button</li>
+			<li><strong>System Response 3</strong> Q-Review opens the set up game screen.</li>
+			<li><strong>User Action 4</strong> DeepDiveDylan......</li>
+			<li><strong>System Response 4</strong> .....</li>
 		</ul>
 		</div>
 
-		<!--here's a div for my ERD:-->
-		<div>
-			<h2>ERD for <em>Medium</em> website</h2>
-			<!--I used an <object> tag to add this svg image to the page, since this was the most recommended option on https://www.sitepoint.com/add-svg-to-web-page/ If you intend using any advanced SVG features such as CSS and scripting, the HTML5 <object> tag is your best option"-->
-			<img src="images/erd.png" alt="Image ERD" />
-		</div>
+
 
 	</body>
 </html>
