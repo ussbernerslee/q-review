e13430f2f321b2106c36247ee73f358f79889e5c
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-30 18:07:39 -0700

    preliminary profile started

--- epic/proctor.php
+++ epic/proctor.php
@@ -6,13 +6,61 @@
 
 	</head>
 	<body>
-		<h1>Persona</h1>
-		<h3>Dylan</h3>
+		<h2>Personas, Use Cases, and Interaction Flow</h2>
+		<h3>Persona:</h3>
+		<ul>
+			<li><strong>Name</strong>: "Deep Dive Dylan"</li>
+			<li><strong>Personality</strong>: </li>
+			<li><strong>Gender</strong>: Male</li>
+			<li><strong>Age</strong>: 35</li>
+			<li><strong>Technology</strong>: mac, pc, linux...but happiest on mac
+				<ul>
+					<li><strong>Device</strong> Newest Macbook Pro with Touch Bar</li>
+					<li><strong>Proficiency</strong> Master</li>
+					<li><strong>Love/Hate</strong> Loves, Loves, LOVES!  Checks Slack continuously. Keeps track of Bitcoin shares.</li>
+				</ul>
+			</li>
+			<li><strong>Attitudes and Needs</strong>
+				<ul>
+					<li><strong>What need does this person have?</strong> He needs a Jeopardy-like game for his students to play.</li>
+
+					<li><strong>Why choose your site over other options?</strong> Medium is beautiful, easy for her family back home to use, and allows commenting, and highlighting directly within the text of the article.</li>
+				</ul>
+			</li>
+		</ul>
+
 		<h3>User Story</h3>
-		<p>As a user, I want to</p>
+		<h4>As a member of Medium, I want to post articles</h4>
+		<p></p>
 		<h3>Use Case</h3>
-		<h3>Preconditions</h3>
-		<h3>Postconditions</h3>
-		<h3>Interaction Flow</h3>
+		<ul>
+			<li><strong>Title: </strong> Through-Hiking the Appalachian Trail</li>
+			<li><strong>Name of the "actor, user or Persona, and their role: </strong> Alicia, Through-Hiker</li>
+			<li><strong>Usage Preconditions: </strong> Alicia needs to be a member of Medium and be signed in (Has to be in a place where she has wifi access)</li>
+			<li><strong>Usage Postconditions:</strong> Alicia's article is posted and available for her friends to read while she continues hiking.</li>
+			<li><strong>Interaction Flow(s), including alternates, if appropriate</strong></li>
+			<li><strong>Frequency of Use: </strong> once-a-week</li>
+		</ul>
+		<h3>Interaction Flow:</h3>
+		<!--Enter each after the </strong> and before the </li>-->
+		<ul>
+			<li><strong>User Action 1</strong> Alicia enters her username and password.</li>
+			<li><strong>System Response 1</strong> Medium logs her in.</li>
+			<li><strong>User Action 2</strong> Alicia clicks that she wants to post an article.</li>
+			<li><strong>System Response 2</strong> Medium opens a box in which she can write or cut and paste her article, with an area for title and for content.</li>
+			<li><strong>User Action 3</strong> Alicia enters her article, with title and content.</li>
+			<li><strong>System Response 3</strong> Medium saves her article and displays it for other members to read.</li>
+			<li><strong>User Action 4</strong> Alicia clicks the logout button.</li>
+			<li><strong>System Response 4</strong> Medium logs Alicia out.</li>
+		</ul>
+		</div>
+
+		<!--here's a div for my ERD:-->
+		<div>
+			<h2>ERD for <em>Medium</em> website</h2>
+			<!--I used an <object> tag to add this svg image to the page, since this was the most recommended option on https://www.sitepoint.com/add-svg-to-web-page/ If you intend using any advanced SVG features such as CSS and scripting, the HTML5 <object> tag is your best option"-->
+			<img src="images/erd.png" alt="Image ERD" />
+		</div>
+
 	</body>
 </html>
