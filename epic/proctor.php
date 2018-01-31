0a15b03e26a51f9cdb2e7fa1e49534c9c5d1e39b
Anna Khamsamran <akamsamran1@cnm.edu>
2018-01-30 17:55:14 -0700

    added sections for persona

--- epic/proctor.php
+++ epic/proctor.php
@@ -2,10 +2,17 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
-		<title>snap-css-3</title>
+		<title>Proctor Persona</title>
 
 	</head>
 	<body>
-		<h1></h1>
+		<h1>Persona</h1>
+		<h3>Dylan</h3>
+		<h3>User Story</h3>
+		<p>As a user, I want to</p>
+		<h3>Use Case</h3>
+		<h3>Preconditions</h3>
+		<h3>Postconditions</h3>
+		<h3>Interaction Flow</h3>
 	</body>
 </html>
