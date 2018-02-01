0a2c3c21f7e73e4d86069107af36af31906e30fb
Kenneth Keyes <kkeyes1@cnm.edu>
2018-02-01 08:38:27 -0700

    created a temporary Epic Doc to aggregate all the different team pieces for easier editing and formatting before breaking them back out into their individual sections

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -7,6 +7,7 @@
 	<body>
 		<h1>Executive Summary</h1>
 		<p>Problem</p>
+		<p>Any number of contestants each log in to the Q-Review game. The contestants compete in a quiz game comprising three rounds: Jeopardy!, Double Jeopardy!, and Final Jeopardy!.[5] The material for the clues covers a wide variety of topics, including history and current events, the sciences, the arts, popular culture, literature, and languages.[6] Category titles often feature puns, wordplay, or shared themes, and the host will regularly remind contestants of topics or place emphasis on category themes before the start of the round.</p>
 		<p>Solution</p>
 		<p>Q Review is a competitive question and answer review game that allows an instructor to create custom flash-cards with unified difficulty, point values, and category tags. With this system of identification, Q Review will allow the instructor to generate a Game Board with any number of categories and questions with increasing difficulty. Q Review then allows multiple Student users to simultaneously join the Game that the instructor has started. Q Review will allow the instructor to moderate a review game, where students must race to hit the spacebar in order to be selected to answer questions in order to be awarded points. Q Review will have the ability to accept written answers from students as well.</p>
 	</body>
