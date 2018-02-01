841983425026963722ee329e8e962e556f2d9f05
Tristan Bennett <tmbennett2011@msn.com>
2018-02-01 12:30:47 -0700

    updated concept model

--- epic/kens-conceptual-model.php
+++ epic/kens-conceptual-model.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<title>Ken's Conceptual Model</title>
+		<meta charset="UTF-8" />
+		<title>Ken's Conceptual Model-Edited AK 1/31/2018 21:31</title>
 	</head>
 	<body>
 
@@ -20,60 +20,65 @@
 		<!-- List of categories tied to the columns in game (CSS, HTML, SQL, etc) -->
 		<h1>Entity: Category</h1>
 		<h3>Attribute: categoryId(Primary Key) BINARY(16) NOT NULL</h3>
-		<h3>Attribute: categoryProfileId (Foreign Key)BINARY(16) NOT NULL</h3>
-		<h3>Attribute: categoryTag VARCHAR(15)</h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
+		<h3>Attribute: categoryTag VARCHAR(15)</h3><!--this is the name of the category-->
+
 
 		<!-- Question card containing question in specific category, answer, and value -->
 		<h1>Entity: Card</h1>
 		<h3>Attribute: cardId(Primary Key) BINARY(16) NOT NULL</h3>
-		<h3>Attribute: cardCategoryId (Foreign Key)BINARY(16) NOT NULL</h3>
-		<h3>Attribute: cardQuestion VARCHAR(500)</h3>
+		<h3>Attribute: cardCategoryId (Foreign Key)BINARY(16) NOT NULL</h3><!--this works if there is only 1 category per card-->
 		<h3>Attribute: cardAnswerVARCHAR(500)</h3>
-		<h3>Attribute: cardValue CHAR(1)</h3>
-		<!--ask bridge: we want this to be assigned a letter A,B,C,D corresponding to comparative ease of question(A=Easiest, D=Hardest)Then when the proctor sets up the game, she/he sets what values they want for each box, and the cards fill the appropriate box in game board-->
-		<h3>Attribute: cardDoubleOrNothing</h3><!--not here-->
-		<h3>Attribute: cardFinalWager</h3><!--not here-->
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
+		<h3>Attribute: cardDifficulty INTEGER (1)1:beginner, 2:intermediate, 3:advanced(?)</h3><!--for Minimum Viable Product, we COULD assign the value of the card here so we don't have to have another weak entity called pointsBoard, however there are many errors that can be associated with doing it this way, rather than using an additional table. The main problem is that if the person who sets up their cards messes up the point system, the columns could have differing point values in matching rows. -->
+		<h3>Attribute: cardQuestion VARCHAR(500)</h3>
+		<h3>Attribute: cardValue CHAR(1)</h3><!--ask bridge: assign fixed number of points vs assign a letter A,B,C,D (or something like this)corresponding to comparative ease of question(A=Easiest, D=Hardest)Then when the proctor sets up the game, she/he sets what values they want for each box, and the cards fill the appropriate box in game board-->
 
 
 		<!-- Created game board filled with questions and values -->
 		<h1>Entity: Board</h1>
 		<h3>Attribute: boardId(Primary Key) BINARY(16) NOT NULL</h3>
 		<h3>Attribute: boardProfileId (Foreign Key)BINARY(16) NOT NULL</h3><!--this is the proctor who created the game-->
-		<h3>Attribute: boardSubject</h3><!--here- we might need a subject table to tie cards to game subjects-->
-		<h3>Attribute: boardCategories</h3><!--This needs to be broken out to a new table-->
-		<h3>Attribute: boardCards</h3><!--this needs to be broken out into a new table-->
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
+		<h3>Attribute: boardName VARCHAR(50)</h3><!--or boardSubject-->
+
 
 		<!-- Created score ledger  -->
 		<h1>Entity: Score</h1>
-		<h3>Attribute: scoreID</h3>
-		<h3>Attribute: scoreBoardID</h3>
-		<h3>Attribute: scoreProfileId</h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
-		<h3>Attribute: </h3>
+		<h3>Attribute: scoreID (NOT a composite key)(Primary Key)</h3><!--ask bridge about this...can it be a simple unique index or does it need to be UUID?-->
+		<h3>Attribute: scoreBoardID (Foreign Key)BINARY(16) NOT NULL</h3>
+		<h3>Attribute: scoreProfileId (Foreign Key)BINARY(16) NOT NULL</h3>
+		<h3>Attribute: scoreCorrect(is this an INTEGER or CHAR? it should be a yes/no)</h3><!--The way we set this should depend on how we intend to calculate the score-->
+		<h3>Attribute: scoreDoubleOrNothing (is this an INTEGER or CHAR? it should be a yes/no)</h3>
+		<h3>Attribute: scoreFinalWager INTEGER of some type</h3>
+		<h3>Attribute: scorePoints INTEGER of some type</h3>
 
 
-		<!--Created weak entity linking cards to board-->
-		<h1>Entity: cardBoard</h1>
-		<h3>Attribute: cardBoardId (CompoundPrimaryKey)BINARY(16) NOT NULL</h3>
-		<h3>Attribute: cardBoardBoardId(Foreign Key) BINARY(16) NOT NULL</h3>
-		<h3>Attribute: cardBoardCardId (Foreign Key)BINARY(16) NOT NULL</h3>
-		<h3>Attribute: </h3>
+		<!-- Created weak entity linking Board to Category (needed because there are more than 1 category/column(and a variable number of columns) to each board to indicate which categories make up the columns of the game board to be populated from cards of the same category)-->
+		<h1>Entity: boardCategory</h1>
+		<h3>Attribute: boardCategoryId (CompositePrimaryKey)BINARY(16) NOT NULL</h3>
+		<h3>Attribute: boardCategoryBoardId(Foreign Key) BINARY(16) NOT NULL</h3>
+		<h3>Attribute: boardCategoryCategoryId (Foreign Key)BINARY(16) NOT NULL</h3>
+		<h3>Attribute: boardCategoryColumn INTEGER(1)</h3><!--this assigns the category to a specific column in the board and doesn't need to be more than a single digit-->
 
 
-		<!-- Created weak entity linking Cards to Category -->
+		<!-- Created weak entity assigning points scheme to specific rows for a specific Board(needed because there are more than 1 rows per board(and a variable number of points set by proctor for each of these)-->
+		<h1>Entity: pointsBoard</h1>
+		<h3>Attribute: pointsBoardId (PrimaryKey) BINARY(16) NOT NULL</h3><!--this CAN be a composite primary key using the foreign key: "pointsBoardBoardId" + "pointsBoardRowNumber"-->
+		<h3>Attribute: pointsBoardBoardId (Foreign Key) BINARY(16) NOT NULL</h3><!--there are going to need to be -->
+		<h3>Attribute: pointsBoardRowNumber</h3>
+		<h3>Attribute: pointsBoardValue</h3>
+
+
+
+
+
+
+
+
+
+		<!--*************Stuff for Extension beyone minimum viable product****************-->
+
+		<!-- Created weak entity linking Cards to Category if we want to assign more than one category per card-->
 		<h1>Entity: cardCategory</h1>
-		<h3>Attribute: cardCategoryId (CompoundPrimaryKey)BINARY(16) NOT NULL</h3>
+		<h3>Attribute: cardCategoryId (CompositePrimaryKey)BINARY(16) NOT NULL</h3>
 		<h3>Attribute: cardCategoryCardId(Foreign Key) BINARY(16) NOT NULL</h3>
 		<h3>Attribute: cardCategoryCategoryId (Foreign Key)BINARY(16) NOT NULL</h3>
 		<h3>Attribute: </h3>
@@ -84,13 +89,5 @@
 		<h3>Attribute: </h3>
 
 
-		<!-- Track stats? -->
-		<h1>Entity: Play-By-Play</h1>
-		<h3>Attribute: playByPlayId</h3>
-		<h3>Attribute: </h3>
 
-		<h1>Entity: Score</h1>
-		<h3>Attribute: </h3>
-
-	</body>
 </html>
