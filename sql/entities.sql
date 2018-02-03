06866f4205014fc3a891de3a8c0ab74d9a43dcfc
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-03 13:08:33 -0700

    sql attributes complete for all tables

--- sql/entities.sql
+++ sql/entities.sql
@@ -21,11 +21,15 @@ CREATE TABLE profile(
 -- category table:
 CREATE TABLE category(
 -- attribute for primary key:
-	categoryID BINARY(16) NOT NULL,
+	categoryId BINARY(16) NOT NULL,
 	-- attribute for foreign key:
-	categoryProfileID BINARY(16) NOT NULL,
+	categoryProfileId BINARY(16) NOT NULL,
 	-- attributes for category;
 	categoryName VARCHAR(64) NOT NULL,
+	-- unique index created:
+	UNIQUE (categoryId),
+	-- create foreign keys and relationships:
+	FOREIGN KEY (categoryProfileId) REFERENCES profile(profileId),
 	-- Primary key:
 	PRIMARY KEY(categoryId)
 );
@@ -39,6 +43,50 @@ CREATE TABLE card(
 	cardAnswer VARCHAR(255) NOT NULL,
 	cardPoints TINYINT UNSIGNED,
 	cardQuestion VARCHAR(255),
+	-- unique index created:
+	UNIQUE (cardId),
+	-- create foreign keys and relationships:
+	FOREIGN KEY (cardCategoryId) REFERENCES category(categoryId),
 	-- Primary Key:
 	PRIMARY KEY(cardID)
+);
+
+-- board table:
+CREATE TABLE board(
+	-- attribute for primary key:
+	boardId BINARY(16) NOT NULL,
+	-- attribute for foreign key:
+	boardProfileId BINARY(16) NOT NULL,
+	-- other attribute for board:
+	boardName VARCHAR(64),
+	-- unique index created:
+	UNIQUE (boardID),
+	-- create foreign key and relationships:
+	FOREIGN KEY (boardProfileId) REFERENCES profile(profileId),
+	-- Primary key:
+	PRIMARY KEY (boardId)
+);
+
+-- ledger table:
+CREATE TABLE ledger(
+	-- attribute for foreign keys:
+	ledgerBoardId BINARY(16) NOT NULL,
+	ledgerCardId BINARY(16) NOT NULL,
+	ledgerProfileId BINARY(16) NOT NULL,
+	-- attributes for ledger:
+	ledgerType TINYINT UNSIGNED,
+	ledgerPoints TINYINT SIGNED,
+	-- unique index created:
+	UNIQUE (ledgerBoardId),
+	UNIQUE (ledgerCardId),
+	UNIQUE (ledgerProfileId),
+	-- create foreign keys and relationships:
+	FOREIGN KEY (ledgerBoardId) REFERENCES board(boardId),
+	FOREIGN KEY (ledgerCardId) REFERENCES card(cardId),
+	FOREIGN KEY (ledgerProfileId) REFERENCES profile(profileId),
+	-- Primary Key (compound key):
+	PRIMARY KEY (ledgerBoardId),
+	PRIMARY KEY (ledgerCardId),
+	PRIMARY KEY (ledgerProfileId)
+
 );
