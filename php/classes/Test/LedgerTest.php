4c847f9f67b1128eab5e2a5c86da0a2899b7a2a7
Tristan Bennett <tmbennett2011@msn.com>
2018-02-07 15:07:49 -0700

    started LedgerTest
    https://app.asana.com/0/search/543106786590584/548573267222644

---
+++ php/classes/Test/LedgerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Edu\Cnm\Kmaru\Test;
+
+use Edu\Cnm\Kmaru\Board;
+use Edu\Cnm\Kmaru\Card;
+use Edu\Cnm\Kmaru\Ledger;
+
+// grab autoload
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid/php");
+
