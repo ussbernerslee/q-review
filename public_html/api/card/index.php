292c6af315e2f0b04a225cf89bec8dffaa902470
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-15 22:00:49 -0700

    card/index.php api enforce user signed in https://app.asana.com/0/543064250979534/561934830035503

--- public_html/api/card/index.php
+++ public_html/api/card/index.php
@@ -5,8 +5,8 @@ require_once dirname(__DIR__,3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\Kmaru\{
 	Card,
-	// use the profile class for testing only
-	Profile
+	// use the Category class for testing only
+	Category
 };
 
 /**
@@ -24,7 +24,7 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
-try{
+try {
 	// grab the mySQL connection
 	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/Kmaru.ini");
 
@@ -34,8 +34,57 @@ try{
 	// sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$cardCategoryId = filter_input(INPUT_GET, "cardCategoryId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$cardAnswer = filter_input(INPUT_GET, "cardAnswer",FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$cardAnswer = filter_input(INPUT_GET, "cardAnswer", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$cardPoints = filter_input(INPUT_GET, "cardPoints", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$cardQuestion = filter_input(INPUT_GET, "cardQuestion", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
+	//make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+	// handle GET request - if id is present, that card is returned, otherwise all cards are returned
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+
+		//get a specific card or all cards and update reply
+		if(empty($id) === false) {
+			$card = Card::getCardByCardId($pdo, $id);
+			if($card !== null) {
+				$reply->data = $card;
+			}
+		} else if(empty($cardCategoryId) === false) {
+			$card = Card::getCardByCardCategoryId($pdo, $cardCategoryId)->toArray();
+			if($card !== null) {
+				$reply->data = $tweet;
+			}
+		} else if(empty($cardAnswer) === false) {
+			$cards = Card::getCardByCardAnswer($pdo, $cardAnswer)->toArray();
+			if($cards !== null) {
+				$reply->data = $cards;
+			}
+		} else if(empty($cardPoints) === false) {
+			$cards = Card::getCardByCardPoints($pdo, $cardPoints)->toArray();
+			if($cards !== null) {
+				$reply->data = $cards;
+			}
+		} else if(empty($cardQuestion) === false) {
+			$cards = Card::getCardByCardQuestion($pdo, $cardQuestion)->toArray();
+			if($cards !== null) {
+				$reply->data = $cards;
+			}
+		} else {
+			$cards = Card::getAllCards($pdo)->toArray();
+			if($cards !== null) {
+				$reply->data = $cards;
+			}
+		}
+
+	} else if($method === "PUT" || $method === "POST") {
+
+		//enforce the user is signed in
+		if(empty($_SESSION["category"]) === true) {
+			throw(new \InvalidArgumentException("you must be logged in to write a card", 401));
+		}
+	}
 }
