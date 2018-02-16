7fe28c18f74579a8c601bc91ba82326a101fc60f
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-15 21:34:42 -0700

    card/index.php api sanitized input https://app.asana.com/0/543064250979534/561934830035503

--- public_html/api/card/index.php
+++ public_html/api/card/index.php
@@ -13,7 +13,29 @@ use Edu\Cnm\Kmaru\{
  * api for the Card class
  *
  * @author Anna Khamsamran <akhamsamran1@cnm.edu>
+ * @author George Kephart <>
  */
 //verify the session status. start session if not active
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
+}
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try{
+	// grab the mySQL connection
+	$pdo = connectToEncryptedMySql("/etc/apache2/capstone-mysql/Kmaru.ini");
+
+	// determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	// sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$cardCategoryId = filter_input(INPUT_GET, "cardCategoryId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$cardAnswer = filter_input(INPUT_GET, "cardAnswer",FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$cardPoints = filter_input(INPUT_GET, "cardPoints", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$cardQuestion = filter_input(INPUT_GET, "cardQuestion", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+}
