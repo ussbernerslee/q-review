52f933a8bb450d21b31681163e9aadb0b7e7f6e2
Tristan Bennett <tmbennett2011@msn.com>
2018-02-19 09:19:20 -0700

    started board api
    https://app.asana.com/0/search/543106786590584/561934830035504

---
+++ public_html/api/board/index.php
@@ -0,0 +1,38 @@
+<?php
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/classes/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\Kmaru\ {};
+
+/**
+ * API for the Board
+ *
+ * @author Tristan Bennett tbennett19@cnm.edu
+ */
+//verify the session, if it is not active start it
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/kmaru.ini");
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+
+}
