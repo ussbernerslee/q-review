8521d22f1697fa06a89c98e1aeb844ee3a56f23d
Tristan Bennett <tmbennett2011@msn.com>
2018-02-19 10:26:48 -0700

    finished board api
    https://app.asana.com/0/search/543106786590584/561934830035504

--- public_html/api/board/index.php
+++ public_html/api/board/index.php
@@ -35,4 +35,100 @@ try {
 
 	//sanitize input
 
+	//id reffering to boardId
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$boardProfileId = filter_input(INPUT_GET, "boardProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$boardName = filter_input(INPUT_GET, "boardName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	// make sure the id is valid
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative, 400"));
+	}
+
+	// handle get request. If id is valid, return name of board and creator
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+		//gets a board by boardId
+		if(empty($id) === false) {
+			$board = Board::getBoardbyBoardId($pdo, $id);
+			if($board !== null) {
+				$reply->data = $board;
+			}
+		} else if(empty($boardProfileId) === false) {
+			$boardProfileId = Board::getBoardByBoardProfileId($pdo, $boardProfileId);
+			if($boardProfileId !== null) {
+				$reply->data = $boardProfileId;
+			}
+		} else if(empty($boardName) === false) {
+			$boardName = Board::getBoardByBoardName($pdo, $boardName);
+			if($boardName !== null) {
+				$reply->data = $boardName;
+			}
+		} else if($method === "PUT") {
+			//enforce that the XSRF token is present in the header
+			verifyXsrf();
+			//enforce the end user has a JWT token
+			//validateJwtHeader();
+			//enforce the user is signed in and only trying to edit their own board
+			if(empty($_SESSION["board"]) === true || $_SESSION["board"]->getBoardId()->toString() !== $id) {
+				throw(new \InvalidArgumentException("You are not allowed to access this board", 400));
+			}
+		}
+		validateJwtHeader();
+		//decode the response from the front end
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+		//retrieve the board to be updated
+		$board = Board::getBoardByBoardId($pdo, $id);
+		if($board === null) {
+			throw(new RuntimeException("Board does not exist", 400));
+		}
+		//board profile id
+		if(empty($requestObject->boardProfileId) === true) {
+			throw(new \InvalidArgumentException ("No board profile", 400));
+		}
+		//board name
+		if(empty($requestObject->boardName) === true) {
+			throw(new \InvalidArgumentException ("No board name present", 400));
+		}
+		$board->setBoardProfileId($requestObject->categoryProfileId);
+		$board->setBoardName($requestObject->categoryName);
+		$board->update($pdo);
+
+		// update reply
+		$reply->message = "Board information updated";
+	} elseif($method === "DELETE") {
+		//verify the XSRF Token
+		verifyXsrf();
+
+		//enforce the end user has a JWT token
+		//validateJwtHeader();
+		$board = Board::getBoardByBoardId($pdo, $id);
+		if($board === null) {
+			throw (new RuntimeException("Board does not exist"));
+		}
+		//enforce the user is signed in and only trying to edit their own board
+		if(empty($_SESSION["board"]) === true || $_SESSION["board"]->getBoardId()->toString() !== $board->getBoardId()->toString()) {
+			throw(new \InvalidArgumentException("You are not allowed to access this Board", 400));
+		}
+
+		validateJwtHeader();
+		//delete the board from the database
+		$board->delete($pdo);
+		$reply->message = "Board Deleted";
+	} else {
+		throw (new InvalidArgumentException("Invalid HTTP request", 400));
+	}
+	// catch any exceptions that were thrown and update the status and message state variable fields
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
 }
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+// encode and return reply to front end caller
+echo json_encode($reply);
