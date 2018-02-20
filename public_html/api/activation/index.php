ef2783c01290b76d1aa5e9929f2ce8869700c8e7
Tristan Bennett <tmbennett2011@msn.com>
2018-02-20 13:42:43 -0700

    changed namespace activation api, finished testing signup api
    https://app.asana.com/0/search/543106786590584/561934830035504

--- public_html/api/activation/index.php
+++ public_html/api/activation/index.php
@@ -3,7 +3,7 @@ require_once dirname(__DIR__,3) . "/vendor/autoload.php";
 require_once dirname(__DIR__,3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__,3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-use Edu\Cnm\q-review\Profile;
+use Edu\Cnm\Kmaru\Profile;
 /**
  * api for the Profile Activation class
  *
@@ -19,7 +19,7 @@ $reply->status = 200;
 $reply->data = null;
 try {
 	//grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/q-review.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/kmaru.ini");
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 	//sanitize activation input
