70f999ba225d21b8c680d74f19b58db59af5ee3f
Tristan Bennett <tmbennett2011@msn.com>
2018-02-22 13:27:59 -0700

    modified POST
    https://app.asana.com/0/search/543106786590584/561934830035504

--- public_html/api/board/index.php
+++ public_html/api/board/index.php
@@ -9,6 +9,8 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\Kmaru\{Board};
 use PubNub\PNConfiguration;
 use PubNub\PubNub;
+
+
 //TODO: decode sucks
 /**
  * Accessing pubnub for the Board
@@ -55,11 +57,11 @@ try {
 	$boardProfileId = filter_input(INPUT_GET, "boardProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	//can be empty
 	$boardName = filter_input(INPUT_GET, "boardName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-//TODO: this is always empty...FAIL
+//TODO: this is always empty or invalid...FAIL
 	// make sure the id is valid
-//	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
-//		throw(new InvalidArgumentException("Id cannot be empty or invalid, 400"));
-//	}
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
+		throw(new InvalidArgumentException("Id cannot be empty or invalid, 400"));
+	}
 
 	// handle get request. If id is valid, return name of board and creator
 	if($method === "GET") {
@@ -79,58 +81,36 @@ try {
 			}
 
 		}
-	}else if($method === "PUT") {
-			//enforce that the XSRF token is present in the header
-			verifyXsrf();
-			//enforce the end user has a JWT token
-			validateJwtHeader();
-			//enforce the user is signed in and only trying to edit their own board
-			if(empty($_SESSION["board"]) === true || $_SESSION["board"]->getBoardId()->toString() !== $id) {
-				throw(new \InvalidArgumentException("You are not allowed to access this board", 400));
-			}
-
-
-
 	} else if($method === "POST") {
 			//enforce that the XSRF token is present in the header
 			verifyXsrf();
 			//enforce the end user has a JWT token
-			validateJwtHeader();
-			//enforce the user is signed in and only trying to edit their own board
-			if(empty($_SESSION["board"]) === true || $_SESSION["board"]->getBoardId()->toString() !== $id) {
+		//decode the response from the front end
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//checking creator of board
+		if(empty($requestObject->boardName) === true) {
+			throw(new \InvalidArgumentException ("No board profile", 400));
+		}
+
+			if(empty($_SESSION["profile"] === true)) {
 				throw(new \InvalidArgumentException("You are not allowed to access this board", 400));
 			}
 
+			$board = new Board(generateUuidV4(), $_SESSION["profile"]->getProfileId(), $requestObject->boardName);
+			//enforce the user is signed in and only trying to edit their own board
+//			if(empty($_SESSION["board"]) === true || $_SESSION["board"]->getBoardId()->toString() !== $id) {
+//				throw(new \InvalidArgumentException("You are not allowed to access this board", 400));
+//			}
+			$_SESSION["boardId"] = $board->getBoardId();
 			// creating channel for board
 			$pubNubBoard->addChannelToChannelGroup()
-				->channels($_SESSION["board"]->getBoardId())
+				->channels($_SESSION["boardId"])
 				->channelGroup("ddc")
 				->sync();
 
 			$reply->message = "Board Created" . $id;
-
-		//enforce the end user has a JWT token
-		validateJwtHeader();
-		//decode the response from the front end
-		$requestContent = file_get_contents("php://input");
-		$requestObject = json_decode($requestContent);
-		//retrieve the board to be updated
-		$board = Board::getBoardByBoardId($pdo, $id);
-		//check to see if board exists
-		if($board === null) {
-			throw(new RuntimeException("Board does not exist", 400));
-		}
-		//checking creator of board
-		if(empty($requestObject->boardProfileId) === true) {
-			throw(new \InvalidArgumentException ("No board profile", 400));
-		}
-		//setting board profile and board name as long as board exists
-		$board->setBoardProfileId($requestObject->boardProfileId);
-		$board->setBoardName($requestObject->boardName);
-		$board->update($pdo);
-
-		// update reply
-		$reply->message = "Board information updated";
 
 
 	} elseif($method === "DELETE") {
