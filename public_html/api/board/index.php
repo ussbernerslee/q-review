6fe66ddb54a52c9f98a84b7d3bc08ccd328b90cb
Tristan Bennett <tmbennett2011@msn.com>
2018-02-27 16:51:51 -0700

    re wrote board api, needs pubnub integration
    https://app.asana.com/0/search/543106786590584/561934830035504

--- public_html/api/board/index.php
+++ public_html/api/board/index.php
@@ -6,27 +6,26 @@ require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\Kmaru\{Profile, Board};
+use Edu\Cnm\Kmaru\{
+	Profile, Board
+};
 use PubNub\PNConfiguration;
 use PubNub\PubNub;
 
-
-//TODO: decode sucks
-/**
- * Accessing pubnub for the Board
- **/
-$config = readConfig("/etc/apache2/capstone-mysql/kmaru.ini");
-$pubNub = json_decode($config["pubnub"]);
-
-$pubNubConf = new PNConfiguration();
-
-$pubNubConf->setSubscribeKey($pubNub->subscribeKey);
-$pubNubConf->setPublishKey($pubNub->publishKey);
-$pubNubConf->setSecretKey($pubNub->secretKey);
-$pubNubConf->setSecure(true);
-
-$pubNubBoard = new PubNub($pubNubConf);
-
+///**
+// * Accessing pubnub for the Board
+// **/
+//$config = readConfig("/etc/apache2/capstone-mysql/kmaru.ini");
+//$pubNub = json_decode($config["pubnub"]);
+//
+//$pubNubConf = new PNConfiguration();
+//
+//$pubNubConf->setSubscribeKey($pubNub->subscribeKey);
+//$pubNubConf->setPublishKey($pubNub->publishKey);
+//$pubNubConf->setSecretKey($pubNub->secretKey);
+//$pubNubConf->setSecure(true);
+//
+//$pubNubBoard = new PubNub($pubNubConf);
 
 
 /**
@@ -47,97 +46,59 @@ $reply->data = null;
 try {
 	//grab the mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/kmaru.ini");
-	$_SESSION["profile"] = Profile::getProfileByProfileEmail($pdo, "tbennett19@cnm.edu");
-	$authObject = (object)["profileId" => $_SESSION["profile"]->getProfileId()->toString()];
-	setJwtAndAuthHeader("authObject", $authObject);
 
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-
-	//sanitize input; id is boardId
+	// sanitize input
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$boardProfileId = filter_input(INPUT_GET, "boardProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	//can be empty
 	$boardName = filter_input(INPUT_GET, "boardName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-//TODO: this is always empty or invalid...FAIL
-	// make sure the id is valid
+	// make sure the id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
-		throw(new InvalidArgumentException("Id cannot be empty or invalid, 400"));
+		throw(new InvalidArgumentException("id cannot be empty or invalid", 405));
 	}
 
-	// handle get request. If id is valid, return name of board and creator
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
-		//gets a board by boardId
+		//gets a post by content
 		if(empty($id) === false) {
-			$board = Board::getBoardbyBoardId($pdo, $id);
+			$board = Board::getBoardByBoardId($pdo, $id);
 			if($board !== null) {
 				$reply->data = $board;
 			}
-			// else gets the board by the profile that crated it
-		} else if(empty($boardProfileId) === false) {
+		} else {
 			$boardProfileId = Board::getBoardByBoardProfileId($pdo, $boardProfileId);
 			if($boardProfileId !== null) {
 				$reply->data = $boardProfileId;
 			}
-
 		}
-	} else if($method === "POST") {
-			//enforce that the XSRF token is present in the header
-			verifyXsrf();
-			//enforce the end user has a JWT token
+	} elseif($method === "POST") {
+		//enforce that the XSRF token is present in the header
+		verifyXsrf();
+		//enforce the end user has a JWT token
+		validateJwtHeader();
+
 		//decode the response from the front end
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
-		//checking creator of board
-		if(empty($requestObject->boardName) === true) {
-			throw(new \InvalidArgumentException ("No board profile", 400));
+		//enforce that an authorized user is trying to create a board
+		if(empty($_SESSION["profile"]) === true) {
+			throw(new \InvalidArgumentException("You are not allowed to create a board", 403));
 		}
 
-//			if(empty($_SESSION["profile"] === true)) {
-//				throw(new \InvalidArgumentException("You are not allowed to access this board", 401));
-//			}
-
-			$board = new Board(generateUuidV4(), $_SESSION["profile"]->getProfileId(), $requestObject->boardName);
+		$board = new Board(generateUuidV4(), $_SESSION["profile"]->getProfileId(), $requestObject->boardName);
 		$board->insert($pdo);
-			//enforce the user is signed in and only trying to edit their own board
-//			if(empty($_SESSION["board"]) === true || $_SESSION["board"]->getBoardId()->toString() !== $id) {
-//				throw(new \InvalidArgumentException("You are not allowed to access this board", 400));
-//			}
-			$_SESSION["boardId"] = $board->getBoardId();
-			// creating channel for board
-			$pubNubBoard->addChannelToChannelGroup()
-				->channels($_SESSION["boardId"])
-				->channelGroup("ddc")
-				->sync();
-
-			$reply->message = "Board Created" . $id;
+		// update reply
+		$reply->message = "Board created";
+	}
 
 
-	} elseif($method === "DELETE") {
-		//verify the XSRF Token
-		verifyXsrf();
-
-		//enforce the end user has a JWT token
-		validateJwtHeader();
-		$board = Board::getBoardByBoardId($pdo, $id);
-		if($board === null) {
-			throw(new RuntimeException("Board does not exist"));
-		}
-		//enforce the user is signed in and only trying to edit their own board
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $board->getBoardProfileId()->toString()) {
-			throw(new \InvalidArgumentException("You are not allowed to access this Board", 400));
-		}
-
-		validateJwtHeader();
-		//delete the board from the database
-		$board->delete($pdo);
-		$reply->message = "Board Deleted";
-	} else {
-		throw (new InvalidArgumentException("Invalid HTTP request", 400));
+	header("Content-type: application/json");
+	if($reply->data === null) {
+		unset($reply->data);
 	}
 	// catch any exceptions that were thrown and update the status and message state variable fields
 } catch(\Exception | \TypeError $exception) {
@@ -145,10 +106,6 @@ try {
 	$reply->message = $exception->getMessage();
 	$reply->trace = $exception->getTrace();
 }
-header("Content-type: application/json");
-if($reply->data === null) {
-	unset($reply->data);
-}
-
 // encode and return reply to front end caller
 echo json_encode($reply);
+
