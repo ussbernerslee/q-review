07cea770ed57d167d2106ac9b49236f1e4a5a595
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-02-28 09:01:31 -0700

    added board and card to use in ledger api

--- public_html/api/ledger/index.php
+++ public_html/api/ledger/index.php
@@ -6,7 +6,8 @@ require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\Kmaru\Ledger;
+use Edu\Cnm\Kmaru\{Ledger, Profile, Board, Card};
+
 
 /**
  *
