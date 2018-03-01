d382357a60d0ff7eee36d04c668a49d8df72f266
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-03-01 10:59:14 -0700

    added senator.service.ts

---
+++ src/app/shared/servies/senator.service.ts
@@ -0,0 +1,21 @@
+// this is from SenatorArlo's Senator Chatroom:
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs/Observable";
+import {Status} from "../classes/status";
+
+@Injectable()
+export class SenatorService {
+
+	constructor(protected http: HttpClient) {}
+
+	private senatorUrl = "api/senator/";
+
+	createRoom(roomName: string) : Observable<Status> {
+		return(this.http.post<Status>(this.senatorUrl, {command: "open", roomName: roomName}));
+	}
+
+	destroyRoom(roomName: string): Observable<Status> {
+		return(this.http.post<Status>(this.senatorUrl, {command: "delete", roomName: roomName}));
+	}
+}
