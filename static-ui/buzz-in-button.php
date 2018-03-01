3c2cb840d38562daa57291f2f79807b1cd60f1bc
Tristan Bennett <tmbennett2011@msn.com>
2018-03-01 11:49:43 -0700

    basic buzz in button

--- static-ui/buzz-in-button.php
+++ static-ui/buzz-in-button.php
@@ -20,6 +20,12 @@
 	</head>
 	</head>
 	<body>
-
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-6 offset-lg-3">
+					<button type="button" class="btn btn-primary btn-lg btn-block mh-5">Buzz In!</button>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
