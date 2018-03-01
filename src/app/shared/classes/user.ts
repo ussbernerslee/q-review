d68261f1d2e4293ad12734c2a1aa934eb02187f2
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-03-01 10:53:24 -0700

    added a TODO note about changes that need to be made to use this file effectively in our project

--- src/app/shared/classes/user.ts
+++ src/app/shared/classes/user.ts
@@ -1,3 +1,4 @@
+// TODO this is a template that can be used for our classes-needs new attributes, etc...
 export class User {
 	constructor(public id: number, public email: string, public phone: string, public name: string) {}
 }
