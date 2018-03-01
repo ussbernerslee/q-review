4b5eedba779b2fb32a3be5d7579cf6548119c7cf
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-03-01 10:57:35 -0700

    added user.constituent.service.ts

---
+++ src/app/shared/servies/constituent.service.ts
@@ -0,0 +1,17 @@
+// this is from SenatorArlo's Senator Chatroom:
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs/Observable";
+import {Status} from "../classes/status";
+
+@Injectable()
+export class ConstituentService {
+
+	constructor(protected http: HttpClient) {}
+
+	private constituentUrl = "api/constituent/";
+
+	joinRoom(roomName: string, username: string) : Observable<Status> {
+		return(this.http.post<Status>(this.constituentUrl, {roomName: roomName, username: username}));
+	}
+}
