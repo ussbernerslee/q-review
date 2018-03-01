54c52d2d8fb06f5182a360dd16bccbe28da25918
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-03-01 10:55:51 -0700

    added user.service.ts, with a TODO note about changes that need to be made to use this file effectively in our project

---
+++ src/app/shared/servies/user.service.ts
@@ -0,0 +1,17 @@
+//TODO need to be sure to update this, as needed for our project
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs/Observable";
+import {User} from "../classes/user";
+
+@Injectable()
+export class UserService {
+
+	constructor(protected http: HttpClient) {}
+
+	private userUrl = "https://jsonplaceholder.typicode.com/users/";
+
+	getAllUsers() : Observable<User[]> {
+		return(this.http.get<User[]>(this.userUrl));
+	}
+}
