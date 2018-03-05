e43785b72d20a17bf980b5c152912b47fdf309a7
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-03-05 13:00:54 -0700

    added ledger.service.ts service

---
+++ src/app/shared/services/ledger.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {Status} from "../classes/status";
+import {Ledger} from "../classes/ledger";
+
+@Injectable()
+export class LedgerService {
+	constructor(protected http : HttpClient) {
+
+	}
+
+	private ledgerUrl = "api/ledger/";
+
+
+
+
+	//preform the post to initiate ledger
+	postLedger(ledger:Ledger) : Observable<Status> {
+		return(this.http.post<Status>(this.ledgerUrl, ledger));
+	}
+
+// call to the ledger API and GET ledgerPoints based on its ledgerBoardId
+	getLedger(ledgerBoardId : number) : Observable<Ledger> {
+		return (this.http.get<Ledger>(this.ledgerUrl + ledgerBoardId));
+	}
+
+
+
+}
