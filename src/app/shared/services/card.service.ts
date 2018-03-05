d944cfd8eeaf7260ca84f55a7fb47773bf059df5
Tristan Bennett <tmbennett2011@msn.com>
2018-03-05 12:54:40 -0700

    created card service

---
+++ src/app/shared/services/card.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from "@angular/core";
+
+import {Status} from "../classes/status";
+import {Card} from "../classes/card";
+import {Observable} from "rxjs/Observable";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable ()
+export class CardService {
+
+	constructor(protected http : HttpClient) {}
+
+	// api endpoint
+	private cardUrl = "api/card/";
+
+	// call card API and delete the specific card
+	deleteCard(cardId: number) : Observable<Status> {
+		return(this.http.delete<Status>(this.cardUrl + cardId));
+	}
+	// call card API and update a card
+	editCard(card: Card) : Observable<Status> {
+		return(this.http.put<Status>(this.cardUrl + card.cardId, card));
+	}
+
+	// call card API and create a card
+	createCard(card: Card) : Observable<Status> {
+		return(this.http.post<Status>(this.cardUrl, card));
+	}
+
+	// call card API and get a card by card id
+	getCard(cardId: number) : Observable<Card> {
+		return(this.http.get<Card>(this.cardUrl + cardId));
+	}
+
+	// call card API and get cards by card category id
+	getCardByCardCategoryId(cardCategoryId: number) : Observable<Card[]> {
+		return(this.http.get<Card[]>(this.cardUrl + cardCategoryId));
+	}
+
+	// call card API and get cards by card points
+	getCardByCardPoints(cardPoints: number) : Observable<Card[]> {
+		return(this.http.get<Card[]>(this.cardUrl + cardPoints));
+	}
+}
