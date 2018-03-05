c547970714c2c6945b1c0eede6dbb8c09e0ae040
Anna Khamsamran <akhamsamran1@cnm.edu>
2018-03-05 12:07:37 -0700

    added ledger.ts class

---
+++ src/app/shared/classes/ledger.ts
@@ -0,0 +1,10 @@
+export class Ledger {
+	constructor(
+		public ledgerBoardId: string,
+		public ledgerCardId: string,
+		public ledgerProfileId: string,
+		public ledgerPoints: number,
+		public ledgerType: string,
+
+	) {}
+}
