2f4f43d3f69f2d1c4a4320685b919a27a390d49b
Tristan Bennett <tmbennett2011@msn.com>
2018-03-06 09:05:45 -0700

    added post to category service

--- src/app/shared/services/category.service.ts
+++ src/app/shared/services/category.service.ts
@@ -2,8 +2,9 @@ import {Injectable} from "@angular/core";
 import {HttpClient, HttpParams} from "@angular/common/http";
 
 import {Status} from "../classes/status";
-import {Category from "../classes/category";
+import {Category} from "../classes/category";
 import {Observable} from "rxjs/Observable";
+import {Board} from "../classes/board";
 
 @Injectable ()
 export class CategoryService {
@@ -14,8 +15,13 @@ export class CategoryService {
 	private categoryUrl = "api/category/";
 
 	//call to the category API and create the category This is currently a PUT, but does it need to be a POST?
+	editCategory(category: Category) : Observable<Status> {
+		return(this.http.put<Status>(this.categoryUrl + category.categoryId, category));
+	}
+
+	//call to the board API and create the category
 	createCategory(category: Category) : Observable<Status> {
-		return(this.http.put<Status>(this.categoryUrl , category));
+		return(this.http.post<Status>(this.categoryUrl , category));
 	}
 
 	//call the category API and get a category object by its Id
