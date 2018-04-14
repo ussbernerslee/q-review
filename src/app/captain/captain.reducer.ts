b4a57dd1818265b38819800148e861a803f80eef
Dylan McDonald <dmcdonald21@cnm.edu>
2018-04-14 10:21:53 -0600

    added actions to reducer

--- src/app/captain/captain.reducer.ts
+++ src/app/captain/captain.reducer.ts
@@ -1,9 +1,18 @@
 import {Action, ActionReducer} from "@ngrx/store";
 import {Board} from "../shared/classes/board";
+import {BUZZ_IN, BUZZ_OFF, SCORE, FINAL} from "../shared/classes/actions";
 
 
 export const CaptainReducer: ActionReducer<Board[]> = (state = [], action: Action) => {
 	switch(action.type) {
+		case BUZZ_IN:
+			return state;
+		case BUZZ_OFF:
+			return state;
+		case SCORE:
+			return state;
+		case FINAL:
+			return state;
 		default:
 			return state;
 	}
