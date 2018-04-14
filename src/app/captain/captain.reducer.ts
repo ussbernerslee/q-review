aa8262a26b8a6259e183778a2a7b314378da01ce
Dylan McDonald <dmcdonald21@cnm.edu>
2018-04-14 12:28:22 -0600

    added more reducer methods

--- src/app/captain/captain.reducer.ts
+++ src/app/captain/captain.reducer.ts
@@ -1,16 +1,20 @@
-import {Action, ActionReducer} from "@ngrx/store";
+import {ActionReducer} from "@ngrx/store";
 import {BUZZ_IN, BUZZ_OFF, SCORE, FINAL, CaptainOrder} from "../shared/classes/captain.order";
 
 
 export const CaptainReducer: ActionReducer<any> = (state = [], action: CaptainOrder) => {
 	switch(action.type) {
 		case BUZZ_IN:
+			if(state.buzzes.available === true && state.buzzes.queue.findIndex((player: any) => player.username === action.payload.username) === -1) {
+				let newQueue = state.buzzes.queue.sort((player1: any, player2: any) => Math.sign(player1.timestamp - player2.timestamp));
+				return(Object.assign({}, state, {buzzes: {available: true, queue: newQueue}}));
+			}
 			return state;
 		case BUZZ_OFF:
-			return state;
+			return(Object.assign({}, state, {available: false, queue: []}));
 		case SCORE:
 			if(state.id === action) {
-				return Object.assign({}, state, {leaderboard: action.payload});
+				return(Object.assign({}, state, {leaderboard: action.payload}));
 			}
 		case FINAL:
 			return state;
