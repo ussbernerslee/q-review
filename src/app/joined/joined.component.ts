88267f964e16ffa637ab79d82c382085919dd6a6
Dylan McDonald <dmcdonald21@cnm.edu>
2018-04-15 20:07:49 -0600

    added buzz in capability to player view

--- src/app/joined/joined.component.ts
+++ src/app/joined/joined.component.ts
@@ -1,7 +1,5 @@
-import {Component, OnInit} from "@angular/core";
-import {Status} from "../shared/classes/status";
+import {Component, HostListener, OnInit} from "@angular/core";
 import {PubNubAngular} from "pubnub-angular2";
-import {JoinService} from "../shared/services/join.service";
 import {ActivatedRoute} from "@angular/router";
 
 
@@ -12,15 +10,39 @@ import {ActivatedRoute} from "@angular/router";
 
 export class JoinedComponent implements OnInit {
 	boardId: string;
+	canBuzzIn: boolean = true;
 
 
-	constructor(protected pubnub: PubNubAngular, protected route: ActivatedRoute) {}
+	constructor(protected pubnub: PubNubAngular, protected route: ActivatedRoute) {
+		let parent = this;
+		this.pubnub.init({
+			publishKey: "pub-c-f4761826-34d5-4b1c-8977-ce66a5199a53",
+			subscribeKey: "sub-c-4c7ad82a-141f-11e8-acae-aa071d12b3f5",
+			ssl: true
+		});
+		this.pubnub.addListener({message: function(pubnubMessage: any) {
+				console.log(pubnubMessage);
+			}
+		});
+	}
 
 	ngOnInit(): void {
 		this.boardId = this.route.snapshot.params.boardId;
+		this.pubnub.publish({message: "joined", channel: "kmaru-" + this.boardId});
+		this.pubnub.subscribe({channels: ["kmaru-" + this.boardId]});
 	}
 
 
+	@HostListener("document:keypress", ["$event"])
+	buzzInKeyboardEvent(keyEvent: KeyboardEvent): void {
+		console.log(keyEvent);
+		if(this.canBuzzIn === true && keyEvent.altKey === false && keyEvent.ctrlKey === false && keyEvent.metaKey == false && keyEvent.repeat === false && (keyEvent.key === "f" || keyEvent.key === "F")) {
+			this.buzzIn();
+		}
+	}
 
-
+	buzzIn(): void {
+		this.canBuzzIn = false;
+		this.pubnub.publish({message: localStorage.getItem("username"), channel: "kmaru-" + this.boardId});
+	}
 }
